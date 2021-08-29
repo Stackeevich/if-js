cbfa45e55a3aee9eca4f002d70c3b0af92ef275e
Stackeevich <statskeevich@gmail.com>
2021-08-29 21:39:32 +0300

    fix bugs

--- js/main.js
+++ js/main.js
@@ -1,50 +1,41 @@
 // question 6 ----------------------------------------------------
 var user = "John Doe"
 console.log(user);
-
 var student = "Vlad"
 console.log(student);
-
 user = student
 // in the variable user my name
 console.log(user);
 
 // question 7 ----------------------------------------------------
 
-let test = 1
-test = test + 1
-// 2
-test = test - 1
-// 1
+var test = 1
+test = 1 + '1'
+// 11
+test -= 1
+// 10
 console.log(test);
-
-test = true
+var test = true ;
 // true, if i write false in the console will output false
 console.log(test);
 
 // question 8 ----------------------------------------------------
+
 var result = 1
 var arr = [2, 3, 5, 8]
-
 for (var i = 0; i < arr.length; i++) {
-
-result = result * arr[i];
-
+result *= arr[i];
 }
-
 console.log(result);
 
 // question 9 ----------------------------------------------------
 
 let arr2 = [2, 5, 8, 15, 0, 6, 20, 3]
 for (let i = 0; i < arr2.length; i++) {   
-if (arr2[i] > 5 && arr2[i] < 10)
-console.log(arr2[i])               
+	if (arr2[i] > 5 && arr2[i] < 10)
+		console.log(arr2[i])               
 }
-// console.log(arr2.filter(elem=> elem>5 && elem<10)); another option without "for if"
-
-
-
+// console.log(arr2.filter(elem=> elem>5 && elem<10)); another option without "for , if"
 
 // question 10 ----------------------------------------------------
 
