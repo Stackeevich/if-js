9cb51b564fdff6f10eee88bb719a5ac716ec1145
Stackeevich <statskeevich@gmail.com>
2021-09-08 22:43:17 +0300

    fix mistakes

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 //  ----------------- ex №5 -----------------
 
 function sum(a){
-    var x = a;
+    let x = a;
     return function(b){ return x + b;};
 }
 
@@ -11,11 +11,11 @@ console.log(sum(5)(2));
 
 //  ----------------- ex №6 -----------------
 
-var colors = ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'];
-var p_collect = document.getElementsByTagName('p');
+let colors = ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'];
+let p_collect = document.getElementsByTagName('p');
 
-var p1 = function() {
-	var k = 0;
+let p1 = function() {
+	let k = 0;
 	return function() {
 		this.style.color = colors[k];
 		k++;
@@ -23,6 +23,7 @@ var p1 = function() {
 	}
 }
 
-for (var i = 0; i < p_collect.length; i++) {
+for (let i = 0; i < p_collect.length; i++) {
 	p_collect[i].addEventListener('click', p1());
 }
+
