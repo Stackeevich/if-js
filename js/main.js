a322ce1f2cab1d8044183b11d36e3e8a95587fbf
Stackeevich <statskeevich@gmail.com>
2021-10-25 20:06:01 +0300

    the task 6 is rewritten

--- js/main.js
+++ js/main.js
@@ -9,28 +9,19 @@ function sum(a) {
 
 //  ----------------- ex №6 -----------------
 
+
 const colors = ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'];
-
-const text1 = document.getElementById('text1');
-const text2 = document.getElementById('text2');
-const text3 = document.getElementById('text3');
-
-const changeTextColor = () => {
-	let i = 0;
-
-	return function (event) {
-		event.target.style.color = colors[i];
-		i++;
-		if (i >= colors.length) {
-			i = 0;
-		}
-	}
+const tagP = document.querySelectorAll('p');
+const changeFun = () => {
+  let colNumber = 0;
+  return function (a) {
+    a.target.style.color = colors[colNumber];
+    colNumber++;
+    if (colNumber === colors.length) {
+      colNumber = 0;
+    }
+  };
+};
+for (let i = 0; i < tagP.length; i++) {
+  tagP[i].addEventListener('click', changeFun());
 }
-
-const changeColor1 = changeTextColor();
-const changeColor2 = changeTextColor();
-const changeColor3 = changeTextColor();
-
-text1.addEventListener('click', changeColor1);
-text2.addEventListener('click', changeColor2);
-text3.addEventListener('click', changeColor3);
