f73b46679ff62af6fb064ce7bde5bccce81571ad
Stackeevich <statskeevich@gmail.com>
2021-11-01 19:39:42 +0300

    make task l-14

---
+++ lesson-14/js/main.js
@@ -0,0 +1,20 @@
+const sendFileForm = document.getElementById('form');
+
+const sendFile = async(event) => {
+  event.preventDefault();
+
+  try {
+    const formData = new FormData(sendFileForm);
+    const data = await fetch('https://fe-student-api.herokuapp.com/api/file',
+    {method: 'POST', body: formData}).then(data => data.json());
+    console.log(data);
+
+  } catch (err) {
+    console.log('An error has occurred:', err);
+  }
+};
+
+sendFileForm.addEventListener('submit', sendFile)
+
+
+console.log(this);
